bb3c9b984a2609997c78790f7aaec6cfc51715fb
seongkyulim <96x60812@gmail.com>
2025-02-01 16:34:34 +0900

    feat: comment in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+// export default -> main component to render.
 export default function Home() {
   return (
     <div className={styles.page}>
